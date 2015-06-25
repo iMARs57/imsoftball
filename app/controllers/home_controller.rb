f794928ba9a34b2771d5c88bf17b007288115f03
iMARs57 <ar.chen57@gmail.com>
2015-06-25 09:21:19 +0800

    heroku push test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,17 +3,17 @@ class HomeController < ApplicationController
 	before_action :authenticate_user!, :only => :determine
 	require "ipaddr"
 	
-	def remote_ip	
-    	locIp = IPAddr.new("127.0.0.1")
-		remIP = IPAddr.new(request.remote_ip)
-    	if remIP === locIp
-      		# Hard coded remote address for test
-      		#IPAddr.new("123.45.67.89")
-      		IPAddr.new("140.112.12.34")
-    	else
-      		remIP
-    	end
-  	end
+	def remote_ip
+    locIp = IPAddr.new("127.0.0.1")
+    remIP = IPAddr.new(request.remote_ip)
+    if remIP === locIp
+      # Hard coded remote address for test
+      #IPAddr.new("123.45.67.89")
+      IPAddr.new("140.112.12.34")
+    else
+      remIP
+    end
+  end
 
 	def determine
 		
@@ -25,7 +25,7 @@ class HomeController < ApplicationController
 		# 未授權過的session，則檢查來query本頁面的 IP 為何
   		# 看此 IP 是否為允許範圍內
   		ntuIP = IPAddr.new("140.112.0.0/16")
-  		if (ntuIP === remote_ip)
+      if (ntuIP === remote_ip)
   			#在此 session 標上已授權記號
     		session[:username] = "anonymous"
     		redirect_to :action => "index"
@@ -39,10 +39,12 @@ class HomeController < ApplicationController
 	def index
 		# 若此session無 已授權 標示，導回首頁
   		#if session[:username] == nil
-    	#	redirect_to :action => "determine"
-		#	return
-  		#end
+      #  redirect_to "home/determine"
+      #  return
+  	  #end
       
+      @message = IPAddr.new(request.remote_ip)
+
       # 執行秀出已授權可看的畫面內容
 
       if(Time.now.year < 2011)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   
   devise_for :users
-  root :to => "home#index"
+  root :to => "home#determine"
 
   resources :namecards
   resources :home
